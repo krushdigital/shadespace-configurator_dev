10138e46e3492caf1e029c301dd309bd1a5cf21e
krushdigital <krushcreative@gmail.com>
2025-11-25 15:10:38 +1300

    Updated pricing.ts

--- src/data/pricing.ts
+++ src/data/pricing.ts
@@ -233,7 +233,7 @@ export const EXCHANGE_RATES: { [key: string]: number } = {
 export const CURRENCY_MARKUPS: { [key: string]: number } = {
   'NZD': 1.00,   // 0% markup
   'USD': 1.30,   // 30% markup 
-  'AUD': 1.00,   // 0% markup 
+  'AUD': 0.90,   // -10% markup 
   'GBP': 1.68,   // 68% markup 
   'EUR': 1.652,  // 65.2% markup 
   'CAD': 1.30,  // 30.0% markup 
