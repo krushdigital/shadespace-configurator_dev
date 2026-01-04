1062281b64f87fd453d1fab4882f4de56c536b94
krushdigital <krushcreative@gmail.com>
2026-01-04 18:33:02 +1300

    Updated SaveProgressButton.tsx

--- src/components/SaveProgressButton.tsx
+++ src/components/SaveProgressButton.tsx
@@ -24,7 +24,7 @@ export function SaveProgressButton({ onClick, className = '' }: SaveProgressButt
         variant="outline"
         size="md"
         onClick={onClick}
-        className={`${className} !bg-gradient-to-r !from-[#BFF102]/10 !to-white hover:!bg-[#307C31] !text-[#01312D] hover:!text-white !border-2 !border-[#307C31]`}
+        className={`${className} !bg-gradient-to-r !from-[#BFF102]/10 !to-white hover:!from-[#307C31] hover:!to-[#307C31] !text-[#01312D] hover:!text-white !border-2 !border-[#307C31]`}
         aria-label="Save your progress"
       >
         <svg className="w-4 h-4 mr-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
