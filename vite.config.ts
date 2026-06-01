813e47427a4e4d6b88de127be993e1c3012a72a8
krushdigital <krushcreative@gmail.com>
2026-06-02 07:43:30 +1200

    Switch to esbuild minifier

--- vite.config.ts
+++ vite.config.ts
@@ -45,13 +45,7 @@ export default defineConfig({
     outDir: "../public/shadespace/",
     emptyOutDir: true,
     sourcemap: false,
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: false,
-        drop_debugger: true,
-      },
-    },
+    minify: "esbuild",
     rollupOptions: {
       output: {
         inlineDynamicImports: false,
